3dac68ef72ded29b870edcf910cc77c546431f06
Mitali <kapuriamitali@gmail.com>
2020-04-14 17:54:50 -0700

    Print

---
+++ src/com/print/Print_String.java
@@ -0,0 +1,16 @@
+package com.print;
+
+public class Print_String {
+	
+	static void print(String str)
+	{
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+
+		String str="This string is going to pass to print method.";
+		print(str);		
+	}
+
+}
