92040712ecb305065eb5f1a162cf38c7bf8d1a99
Mitali <kapuriamitali@gmail.com>
2020-04-14 18:34:23 -0700

    totalBill

---
+++ src/com/print/total_bill.java
@@ -0,0 +1,12 @@
+package com.print;
+
+public class total_bill {
+		static void calculation(int a, int b)
+		{
+		 System.out.println("The devision of the number:" + (a/b));
+		}
+		public static void main(String[] args)
+		{
+		   calculation(10,5);		   
+		}	
+	}
